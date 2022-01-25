6581c7f3e037e0d3ae431ad1701d8c1cb730a47a
rwenmax <rwenmax@gmail.com>
2022-01-25 15:47:37 +0000

    Added base project structure

---
+++ src/main/java/com/sparta/team3/CyberSecurityProjectTeam3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberSecurityProjectTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberSecurityProjectTeam3Application.class, args);
+    }
+
+}
