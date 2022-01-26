8227f70f4491da338974c736c413e9c6a92d5e4a
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2022-01-26 16:03:00 +0000

    changed format

--- terraform/modules/subnets/subnet-outputs.tf
+++ terraform/modules/subnets/subnet-outputs.tf
@@ -6,10 +6,12 @@ output "output_subnet_database_id_tf" {
   value = "${aws_subnet.java10x_cyberg3_subnet_db_tf.id}"
 }
 
+output "output_subnet_proxy_id_tf" {
+  value = "${aws_subnet.java10x_cyberg3_subnet_proxy_tf.id}"
+}
+
+
 output "output_subnet_bastion_id_tf" {
   value = "${aws_subnet.java10x_cyberg3_subnet_bastion_tf.id}"
 }
 
-output "output_subnet_proxy_id_tf" {
-  value = "${aws_subnet.java10x_cyberg3_subnet_proxy_tf.id}"
-}
