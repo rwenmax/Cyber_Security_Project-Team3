cfe4667440a3a12d06b63aeb6bd5deda87d8f1cf
rwenmax <rwenmax@gmail.com>
2022-01-26 17:02:46 +0000

    Fixed issues

--- src/main/java/com/sparta/team3/repositories/TokenRepository.java
+++ src/main/java/com/sparta/team3/repositories/TokenRepository.java
@@ -2,11 +2,13 @@ package com.sparta.team3.repositories;
 
 import com.sparta.team3.entities.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<Token, Integer>
 {
     Optional<Token> findByToken(String token);
-    Optional<Token> findByProfileUserName(String user_id);
+    Optional<Token> findByProfile(String username);
 }
--- src/main/java/com/sparta/team3/repositories/UserProfileRepository.java
+++ src/main/java/com/sparta/team3/repositories/UserProfileRepository.java
@@ -2,9 +2,11 @@ package com.sparta.team3.repositories;
 
 import com.sparta.team3.entities.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
-    Optional<UserProfile> findByUserID(int user_id);
+    Optional<UserProfile> findByid(int id);
 }
